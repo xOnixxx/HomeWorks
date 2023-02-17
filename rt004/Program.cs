09a5f1185723f44ef271f02f98a45fbc4ce6671a
xOnixxx <romanfratrik5@gmail.com>
2023-02-17 16:41:17 +0100

    Add arguments reading

--- rt004/Program.cs
+++ rt004/Program.cs
@@ -3,34 +3,97 @@
 
 namespace rt004
 {
-  internal class Program
-  {
-    static void Main(string[] args)
+
+    enum Mode
     {
-      // Parameters.
-      // TODO: parse command-line arguments and/or your config file.
-      int wid = 600;
-      int hei = 450;
-      string fileName = "demo.pfm";
-
-      // HDR image.
-      FloatImage fi = new FloatImage(wid, hei, 3);
-
-      // TODO: put anything interesting into the image.
-      // TODO: use fi.PutPixel() function, pixel should be a float[3] array [R, G, B]
-
-      for (int x = 0; x < wid; x++)
-      {
-        for (int y = 0; y < hei; y++)
-        {
-            float[] tempC = new float[] {(x*y)%255, (x*x)%255, (y*y)%255};
-            fi.PutPixel(x, y, tempC);
-        }
-      }
-      //fi.SaveHDR(fileName);   // Doesn't work well yet...
-      fi.SavePFM(fileName);
-
-      Console.WriteLine("HDR image is finished.");
+        FileMode,
+        ConsoleMode,
     }
-  }
+
+    enum ImageFormat
+    {
+        HDR,
+        PFM
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wid = 600;
+            int hei = 450;
+            string fileName = "demo.pfm";
+            ImageFormat format = ImageFormat.PFM;
+            Mode mode = Mode.ConsoleMode;
+            // Parameters.
+            // TODO: parse command-line arguments and/or your config file.
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Using defualt values");
+            }
+            if (args.Length is > 0)
+            {
+                //Read file mode, takes no further arguments
+                if (args[0] == "F" && args.Length == 1)
+                {
+                    //TODO: read info from file;
+                }
+                //Read arguments from command line, takes additional 3 arguments
+                else if (args[0] == "C" && args.Length is  <= 4 and >= 3)
+                {
+                    try
+                    {
+                        wid = int.Parse(args[1]);
+                        hei = int.Parse(args[2]);
+                    }
+                    catch
+                    {
+                        throw new Exception("Invalid resolution arguments.");
+                    }
+                    if (args.Length > 3)
+                    {
+                        if (args[3].ToLower() == "hdr")
+                        {
+                            format = ImageFormat.HDR;
+                            fileName = "demo.hdr";
+                        }
+                        else if (args[3].ToLower() == "pfm")
+                        {
+                            format = ImageFormat.PFM;
+                            fileName = "demo.pfm";
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(args[0]);
+                    throw new Exception("Wrong arguments");
+                }
+            }
+
+            // HDR image.
+            FloatImage fi = new FloatImage(wid, hei, 3);
+            // TODO: put anything interesting into the image.
+            // TODO: use fi.PutPixel() function, pixel should be a float[3] array [R, G, B]
+            for (int x = 0; x < wid; x++)
+            {
+                for (int y = 0; y < hei; y++)
+                {
+                    float[] tempC = new float[] { (x * y) % 255, (x * x) % 255, (y * y) % 255 };
+                    fi.PutPixel(x, y, tempC);
+                }
+            }
+
+            if (format == ImageFormat.HDR)
+            {
+                fi.SaveHDR(fileName);
+                Console.WriteLine("HDR Image generated");
+            }
+            else if (format == ImageFormat.PFM)
+            {
+                fi.SavePFM(fileName);
+                Console.WriteLine("PFM Image generated");
+            }
+        }
+    }
 }
