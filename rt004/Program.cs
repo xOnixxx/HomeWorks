c237ebd037f51a8dbb1bb1f242cb204f3dc02723
xOnixxx <romanfratrik5@gmail.com>
2023-02-17 14:19:30 +0100

    Add rt004 template
    Template for first task from RT004

---
+++ rt004/Program.cs
@@ -0,0 +1,28 @@
+﻿using Util;
+//using System.Numerics;
+
+namespace rt004
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      // Parameters.
+      // TODO: parse command-line arguments and/or your config file.
+      int wid = 600;
+      int hei = 450;
+      string fileName = "demo.pfm";
+
+      // HDR image.
+      FloatImage fi = new FloatImage(wid, hei, 3);
+
+      // TODO: put anything interesting into the image.
+      // TODO: use fi.PutPixel() function, pixel should be a float[3] array [R, G, B]
+
+      //fi.SaveHDR(fileName);   // Doesn't work well yet...
+      fi.SavePFM(fileName);
+
+      Console.WriteLine("HDR image is finished.");
+    }
+  }
+}
